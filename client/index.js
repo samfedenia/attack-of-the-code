3a7ec47c3f09bcd2cb4f8461e3693ca98c841061
Sam Fedenia <sam@fedenia.xyz>
2021-05-22 16:50:48 -0500

    added socket connection

--- client/index.js
+++ client/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router } from 'react-router-dom';
 import App from './app';
+import { SocketContext, socket } from './components/context/socket';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <SocketContext.Provider value={socket}>
+    <Router>
+      <App />
+    </Router>
+  </SocketContext.Provider>,
   document.getElementById('app')
 );
