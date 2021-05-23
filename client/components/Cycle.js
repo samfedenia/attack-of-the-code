20a0e32f64a24a34a6bf1a5ff130a69c325b5d3f
danny-zero <sifuarrow@gmail.com>
2021-05-23 12:47:37 -0400

    added random character functionality

--- client/components/Cycle.js
+++ client/components/Cycle.js
@@ -2,19 +2,8 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import styles from './css/Cycle.module.css';
 
-const Cycle = () => {
+const Cycle = ({ headshots, user, setUser, num, setNum }) => {
     const [wobble, setWobble] = useState(0)
-    const [headshots, setHeadshots] = useState([]);
-    const [num, setNum] = useState(0)
-    const [user, setUser] = useState({
-        username: 'test',
-        avatar: ''
-    });
-
-    const getImages = async () => {
-        const {data: images} = await axios.get('/api/headshots');
-        return images
-    };
 
     const goRight = (num) => {
         if (num === headshots.length - 1) {
@@ -40,21 +29,9 @@ const Cycle = () => {
             setUser({...user, avatar: headshots[num]})
         }
     }
-
-    useEffect(() => {
-        getImages()
-            .then((images) => {
-                setHeadshots(images)
-            })
-    }, [])
-
-    // console.log(headshots)
-    // console.log(user)
+    console.log(user)
     return (
         <>
-        {/* <form className={styles.name}>
-        <input type='text' placeholder='name' />
-        </form> */}
         <div className={styles.avatar}
                 onAnimationEnd={() => setWobble(0)}
                 wobble={wobble}>
@@ -62,13 +39,6 @@ const Cycle = () => {
             <div 
                 className={styles.innerAvatar}
                 style={{backgroundImage: `url(/star_wars_characters/${headshots[num]})`}}></div> 
-            {/* <img 
-                onClick={() => setWobble(1)}
-                onAnimationEnd={() => setWobble(0)}
-                wobble={wobble}
-                src={headshots.length > 0 ? `/star_wars_characters/${headshots[num]}` : null} 
-                
-            /> */}
             <button className={styles.arrow} onClick={() => goRight(num)}>{`>`}</button>          
         </div>
         </>
