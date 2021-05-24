ef200114f452c57ee7b487c384f5549783020af6
JonathanCrider <13684007+JonathanCrider@users.noreply.github.com>
2021-05-24 16:16:19 -0400

    started challenges info

---
+++ server/challenges.js
@@ -0,0 +1,31 @@
+// difficulty: padawan, apprentice, master, sith, jedi
+
+const demo = [
+  {
+    prompt: 'Creat a function (hello) that returns the STRING "Hello World!"',
+    start: `const hello () => {
+
+    }`,
+    test1input: 'hello()',
+    test1result: 'Hello World!'
+  }
+];
+
+const padawan = [];
+
+const apprentice = [];
+
+const master = [];
+
+const sith = [];
+
+const jedi = [];
+
+module.exports = {
+  demo,
+  padawan,
+  apprentice,
+  master,
+  sith,
+  jedi
+};
