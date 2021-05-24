53661c08eea81c07d46b1205d20fcb5107ae7898
JonathanCrider <13684007+JonathanCrider@users.noreply.github.com>
2021-05-24 18:08:22 -0400

    added comments explaining the thought process

--- server/challenges.js
+++ server/challenges.js
@@ -1,13 +1,23 @@
 // difficulty: padawan, apprentice, master, sith, jedi
 
+/**
+ * prompt is of course, the prompt
+ * start is to give the player a starting point, that way we populate the correct function name which would
+ *  help avoid errors running the function tests due to a misspelled function
+ *  ~~note, it is a template literal for formatting. this may or may not work/be necessary
+ * testinput is the call
+ * testresult is the expected value of the matching call
+ * */
+
+
 const demo = [
   {
     prompt: 'Creat a function (hello) that returns the STRING "Hello World!"',
     start: `const hello () => {
 
     }`,
-    test1input: 'hello()',
-    test1result: 'Hello World!'
+    testInput1: 'hello()',
+    testResult1: 'Hello World!'
   }
 ];
 
