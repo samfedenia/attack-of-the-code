434d655b737cb918d78c42eccd7b01b5e0add949
danny-zero <sifuarrow@gmail.com>
2021-05-28 21:06:55 -0400

    added newest version of editor

---
+++ client/components/CodePenClone.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect, useContext } from "react";
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+import 'codemirror/mode/javascript/javascript';
+import { Controlled as ControlledEditor } from 'react-codemirror2';
+import 'codemirror/addon/edit/closebrackets';
+import 'codemirror/addon/edit/matchbrackets';
+import { BackgroundContext } from './context/background';
+
+const CodePenClone = ({ value, onChange }) => {
+    const [backgrounds, setBackgrounds] = useContext(BackgroundContext);
+    const [theme, setTheme] = useState('material');
+
+    const handleChange = (editor, data, value) => {
+        //console.log(value)
+        onChange(value)
+    }
+
+    const changeTheme = () => {
+        if (theme === 'material') {
+        setTheme('default')
+        document.body.style.backgroundImage = `url(/jedi-prof.png), linear-gradient(rgba(5, 8, 46, 0.712), rgba(53, 0, 0, 0.801))`;
+        } else {
+        setTheme('material');
+        const randomNum = Math.floor(Math.random() * backgrounds.length);
+        document.body.style.backgroundImage = `url(/backgrounds/${backgrounds[randomNum]}), linear-gradient(rgba(5, 8, 46, 0.712), rgba(53, 0, 0, 0.801))`;
+        }
+  }
+
+
+    return (
+        <>
+        <div className='switch'>
+          <label
+            style={{
+              fontFamily: 'StarJedi',
+              textShadow: 'black 0px 0px 2px',
+              color: 'white',
+              letterSpacing: '.1em',
+            }}
+          >
+            Dark Side
+            <input onClick={changeTheme} type='checkbox' />
+            <span className='lever'></span>
+            Prof Side
+          </label>
+        </div>
+        <div className="editor-container">
+            <ControlledEditor 
+                onBeforeChange={handleChange}
+                value={value}
+                className="code-mirror-wrapper"
+                options={{
+                    lineWrapping: true,
+                    lint: true,
+                    mode: 'javascript',
+                    theme: theme,
+                    lineNumbers: true,
+                    autoCloseBrackets: true,
+                    matchBrackets: true
+                }}
+            />
+        </div>
+        </>
+    )
+}
+
+export default CodePenClone
--- client/components/Game.js
+++ client/components/Game.js
@@ -17,19 +17,71 @@ import Cycle from './Cycle';
 import styles from './css/Game.module.css';
 import { SocketContext } from './context/socket';
 import Prompt from './Prompt';
-import Editor from './Editor';
+import CodePenClone from './CodePenClone';
+import NewCodePenClone from './NewCodePenClone';
 
 const Game = () => {
   // // socket connection logic
   // const socket = useContext(SocketContext);
   // const newRoomCode = Math.random().toString(36).slice(-5);
   // socket.emit("room", newRoomCode, "anon");
+    const [js, setJs] = useState('');
+    const [srcDoc, setSrcDoc] = useState('');
+    const [result, setResult] = useState('');
+
+    const runCode = () => {
+        // console.log(js)
+        setResult(eval(js))
+        setSrcDoc(`
+            <html>
+              <style>
+                * {
+                  font-family: Verdana;
+                }
+              </style>
+                <body>
+                    <div id="result"></div>
+                </body>
+                <script>
+                    const result = document.getElementById('result');
+                    result.innerText = '${eval(js)}'
+                </script>
+            </html>
+        `)
+    };
+
+    const checkCode = () => {
+        if (result === 2) {
+            alert('The answer was 2 you are correct')
+        } else {
+            alert('incorrect')
+        }
+    }
+
+    //console.log('result', result)
+  
 
   return (
     <div className={styles.game}>
-      <Prompt />
-      <Editor />
-      <h1>game</h1>
+        <Prompt />
+        <CodePenClone 
+            value={js}
+            onChange={setJs}
+        />
+        <div className='run-btn'>
+          <button onClick={runCode}>Run</button>
+          <button onClick={checkCode}>Submit</button>
+        </div>
+        <div className="pane">
+            <iframe 
+                srcDoc={srcDoc}
+                title="output"
+                sandbox="allow-scripts"
+                frameBorder="0"
+                width="100%"
+                height="100%"
+            />
+        </div>
     </div>
   );
 };
