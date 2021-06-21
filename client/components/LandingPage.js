5ada466b914c3d6638066e24394dcf5a7914ad76
danny-zero <sifuarrow@gmail.com>
2021-06-20 22:02:41 -0400

    css fixes

--- client/components/LandingPage.js
+++ client/components/LandingPage.js
@@ -200,6 +200,7 @@ const LandingPage = () => {
               fontFamily: font,
               textShadow: 'black 0px 0px 2px',
               letterSpacing: '.1em',
+              backgroundColor: 'transparent'
             }}
             className='blue-border'
           />
--- client/components/Podium.js
+++ client/components/Podium.js
@@ -12,39 +12,41 @@ const Podium = ({ playerList }) => {
 
   console.log("Podium Player List:", playerList);
   return (
-    <div className={styles.podiumContainer}>
-      <h1 style={{ color: "yellow" }}>
+    <div className={styles.outerContainer}>
+      <h1 className={styles.winner} style={{ color: "yellow" }}>
         Winner is {topThree[0]?.playerName}!!!!
       </h1>
-      <div className={styles.standContainer}>
-        <div className={styles.avatarContainer}>
-          <img
-            className={styles.avatar}
-            src={`/star_wars_characters/${topThree[1]?.avatar}`}
-          />
+      <div className={styles.podiumContainer}>
+        <div className={styles.standContainer}>
+          <div className={styles.avatarContainer}>
+            <img
+              className={styles.avatar}
+              src={`/star_wars_characters/${topThree[1]?.avatar}`}
+            />
+          </div>
+          <div id={styles.second} className={styles.stand}></div>
         </div>
-        <div id={styles.second} className={styles.stand}></div>
-      </div>
 
-      <div className={styles.standContainer}>
-        <div className={styles.avatarContainer}>
-          <img
-            className={styles.avatar}
-            src={`/star_wars_characters/${topThree[0]?.avatar}`}
-          />
+        <div className={styles.standContainer}>
+          <div className={styles.avatarContainer}>
+            <img
+              className={styles.avatar}
+              src={`/star_wars_characters/${topThree[0]?.avatar}`}
+            />
+          </div>
+          <div id={styles.first} className={styles.stand}></div>
         </div>
-        <div id={styles.first} className={styles.stand}></div>
-      </div>
 
-      <div className={styles.standContainer}>
-        <div className={styles.avatarContainer}>
-          <img
-            className={styles.avatar}
-            src={`/star_wars_characters/${topThree[2]?.avatar}`}
-          />
+        <div className={styles.standContainer}>
+          <div className={styles.avatarContainer}>
+            <img
+              className={styles.avatar}
+              src={`/star_wars_characters/${topThree[2]?.avatar}`}
+            />
+          </div>
+          <div id={styles.third} className={styles.stand}></div>
         </div>
-        <div id={styles.third} className={styles.stand}></div>
-      </div>
+        </div>
     </div>
   );
 };
