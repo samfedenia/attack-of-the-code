7f04c7377cd9eb401338a1b96cea47a3f45a3358
JonathanCrider <13684007+JonathanCrider@users.noreply.github.com>
2021-06-24 14:27:48 -0400

    fixed error where between wouldnt advance with only 3 players

--- client/components/Between.js
+++ client/components/Between.js
@@ -1,12 +1,12 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { quotesp } from '../quotes';
-import styles from './css/Game.module.css';
-import changeCubeStyles from './css/LandingPage.module.css';
-import { GameContext, GAME_ACTIONS } from './context/game';
-import { UserContext, USER_ACTIONS } from './context/user';
-import { SocketContext } from './context/socket';
+import React, { useContext, useEffect, useState } from "react";
+import { quotesp } from "../quotes";
+import styles from "./css/Game.module.css";
+import changeCubeStyles from "./css/LandingPage.module.css";
+import { GameContext, GAME_ACTIONS } from "./context/game";
+import { UserContext, USER_ACTIONS } from "./context/user";
+import { SocketContext } from "./context/socket";
 // import { Container, Row, Button } from 'react-materialize';
-import MiniGame from './MiniGame';
+import MiniGame from "./MiniGame";
 
 import {
   Modal,
@@ -24,8 +24,8 @@ import {
   Navbar,
   NavDropdown,
   FormControl,
-} from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Between = ({ submissionState, setSubmissionState }) => {
   const randomize = () => Math.floor(Math.random() * quotesp.length);
@@ -46,13 +46,12 @@ const Between = ({ submissionState, setSubmissionState }) => {
   const setGame = () => {
     const newGameState = {
       ...gameState,
-      gameStatus: 'playing',
+      gameStatus: "playing",
       currentRound: gameState.currentRound + 1,
       roundComplete: false,
     };
     gameDispatch({ type: GAME_ACTIONS.SET_GAME, payload: newGameState });
-    socket.emit('new-game-state', newGameState, userState.roomCode);
-
+    socket.emit("new-game-state", newGameState, userState.roomCode);
   };
 
   useEffect(() => {
@@ -86,7 +85,7 @@ const Between = ({ submissionState, setSubmissionState }) => {
             }}
           />
         </Row>
-        <Row hidden={!gameState.roundComplete}>
+        <Row hidden={gameState.roundComplete}>
           <h3>Next Round Starting in {count}</h3>
           {/* <Button onClick={setGame}>Next</Button> */}
         </Row>
