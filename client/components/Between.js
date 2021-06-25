66e67ebddb22bafa7a61d7b3008fd4c4e91fdffd
JonathanCrider <13684007+JonathanCrider@users.noreply.github.com>
2021-06-24 20:56:42 -0400

    reimplemented the button to start next round

--- client/components/Between.js
+++ client/components/Between.js
@@ -1,12 +1,12 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { quotesp } from '../quotes';
-import styles from './css/Game.module.css';
-import changeCubeStyles from './css/LandingPage.module.css';
-import { GameContext, GAME_ACTIONS } from './context/game';
-import { UserContext, USER_ACTIONS } from './context/user';
-import { SocketContext } from './context/socket';
+import React, { useContext, useEffect, useState } from "react";
+import { quotesp } from "../quotes";
+import styles from "./css/Game.module.css";
+import changeCubeStyles from "./css/LandingPage.module.css";
+import { GameContext, GAME_ACTIONS } from "./context/game";
+import { UserContext, USER_ACTIONS } from "./context/user";
+import { SocketContext } from "./context/socket";
 // import { Container, Row, Button } from 'react-materialize';
-import MiniGame from './MiniGame';
+import MiniGame from "./MiniGame";
 
 import {
   Modal,
@@ -24,8 +24,8 @@ import {
   Navbar,
   NavDropdown,
   FormControl,
-} from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Between = ({ submissionState, setSubmissionState }) => {
   const randomize = () => Math.floor(Math.random() * quotesp.length);
@@ -46,35 +46,35 @@ const Between = ({ submissionState, setSubmissionState }) => {
   const setGame = () => {
     const newGameState = {
       ...gameState,
-      gameStatus: 'playing',
+      gameStatus: "playing",
       currentRound: gameState.currentRound + 1,
       roundComplete: false,
     };
     gameDispatch({ type: GAME_ACTIONS.SET_GAME, payload: newGameState });
-    socket.emit('new-game-state', newGameState, userState.roomCode);
+    socket.emit("new-game-state", newGameState, userState.roomCode);
   };
 
-  useEffect(() => {
-    if (gameState.roundComplete) countDown();
-  }, [gameState.roundComplete]);
+  // useEffect(() => {
+  //   if (gameState.roundComplete) countDown();
+  // }, [gameState.roundComplete]);
 
-  const countDown = () => {
-    const countingDown = setInterval(() => {
-      setCount(count - 1);
-      if (count === 0) {
-        clearInterval(countingDown);
-        setGame();
-      }
-    }, 1000);
-  };
+  // const countDown = () => {
+  //   const countingDown = setInterval(() => {
+  //     setCount(count - 1);
+  //     if (count === 0) {
+  //       clearInterval(countingDown);
+  //       setGame();
+  //     }
+  //   }, 1000);
+  // };
 
   return (
     <Container className={styles.container}>
       <div
         style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
         }}
       >
         <div className={styles.profQuotes}>
@@ -92,8 +92,8 @@ const Between = ({ submissionState, setSubmissionState }) => {
           />
         </Row>
         <Row hidden={gameState.roundComplete}>
-          <h3>Next Round Starting in {count}</h3>
-          {/* <Button onClick={setGame}>Next</Button> */}
+          {/* <h3>Next Round Starting in {count}</h3> */}
+          <Button onClick={setGame}>Next</Button>
         </Row>
         <Row>
           <MiniGame />
