c6afc921601ed3318ae64a27c77c3c2aa0a052eb
Sam Fedenia <sam@fedenia.xyz>
2021-06-29 20:28:14 -0500

    center chat input

--- client/components/MiniGame.js
+++ client/components/MiniGame.js
@@ -1,6 +1,6 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { Button } from 'react-bootstrap';
-import styles from './css/Editor.module.css';
+import React, { useState, useRef, useEffect } from "react";
+import { Button } from "react-bootstrap";
+import styles from "./css/Editor.module.css";
 
 const MiniGame = () => {
   const [count, setCount] = useState(0);
@@ -11,21 +11,21 @@ const MiniGame = () => {
 
   const jump = async () => {
     console.log(character.current);
-    if (character.classList !== 'animate') {
-      const jump = await character.current.classList.add('animate');
+    if (character.classList !== "animate") {
+      const jump = await character.current.classList.add("animate");
       setTimeout(function () {
-        character.current.classList.remove('animate');
+        character.current.classList.remove("animate");
       }, 450);
       setCount(count + 1);
       if (count >= 5 && count <= 10) {
-        block.classList.remove('stormtrooperOne');
-        document.getElementById('block').style.width = '45px';
-        block.classList.add('stormtrooperTwo');
+        block.classList.remove("stormtrooperOne");
+        document.getElementById("block").style.width = "45px";
+        block.classList.add("stormtrooperTwo");
       }
       if (count > 10) {
-        block.classList.remove('stormtrooperTwo');
-        document.getElementById('block').style.width = '45px';
-        block.classList.add('stormtrooperThree');
+        block.classList.remove("stormtrooperTwo");
+        document.getElementById("block").style.width = "45px";
+        block.classList.add("stormtrooperThree");
       }
       return jump;
     }
@@ -34,15 +34,14 @@ const MiniGame = () => {
   useEffect(() => {
     let interval = setInterval(() => {
       let characterTop = parseInt(
-        window.getComputedStyle(character.current).getPropertyValue('top')
+        window.getComputedStyle(character.current).getPropertyValue("top")
       );
       let blockLeft = parseInt(
-        window.getComputedStyle(block.current).getPropertyValue('left')
+        window.getComputedStyle(block.current).getPropertyValue("left")
       );
-      console.log('CHARACTER TOP', characterTop);
-      console.log('BLOCK LEFT', blockLeft);
+
       if (blockLeft < 50 && blockLeft > 20 && characterTop >= 150) {
-        block.current.style.animation = 'none';
+        block.current.style.animation = "none";
         setGameOn(false);
         return clearInterval(interval);
       }
@@ -53,7 +52,7 @@ const MiniGame = () => {
   }, [gameOn]);
 
   const onSpacebar = (e) => {
-    if (e.key === ' ') {
+    if (e.key === " ") {
       jump();
     }
   };
@@ -67,32 +66,32 @@ const MiniGame = () => {
     <div>
       {!gameOn ? (
         <div>
-          <div id="game">
+          <div id='game'>
             <Button className={styles.submitBtn} onClick={startMiniGame}>
               Start Mini-Game
             </Button>
             <h2
               style={{
-                color: '#fff103',
-                font: 'StarJedi',
+                color: "#fff103",
+                font: "StarJedi",
               }}
             >
               Final Score: {count}
             </h2>
-            <div id="character"></div>
-            <div id="block"></div>
+            <div id='character'></div>
+            <div id='block'></div>
           </div>
         </div>
       ) : (
         <div
-          id="game"
+          id='game'
           onClick={() => jump()}
           onKeyDown={onSpacebar}
-          tabIndex="0"
+          tabIndex='0'
         >
-          <h2 id="counter">{count}</h2>
-          <div ref={character} id="character" className="bb8"></div>
-          <div ref={block} id="block" className="stormtrooperOne"></div>
+          <h2 id='counter'>{count}</h2>
+          <div ref={character} id='character' className='bb8'></div>
+          <div ref={block} id='block' className='stormtrooperOne'></div>
         </div>
       )}
     </div>
